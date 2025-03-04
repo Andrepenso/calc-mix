11b4cb627c60d5657f5276947bd23f6f3e30f5ce
Andrepenso <70725958+Andrepenso@users.noreply.github.com>
2025-03-04 09:49:20 -0300

    Corrigindo erro Not Found ao atualizar a página

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 5173,
+  },
+  base: "/", // 🔹 Corrige o erro "Not Found" ao atualizar a página
+  build: {
+    outDir: "dist", // 🔹 Define a pasta de saída do build
+  }
+});
