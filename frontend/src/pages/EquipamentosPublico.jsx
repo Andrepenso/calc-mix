5b10adf493132834a597c45acb1617f0965a8af4
Andrepenso <70725958+Andrepenso@users.noreply.github.com>
2025-03-04 13:41:04 -0300

    Corrigindo erro pagina equipamentos

--- frontend/src/pages/EquipamentosPublico.jsx
+++ frontend/src/pages/EquipamentosPublico.jsx
@@ -11,7 +11,7 @@ function EquipamentosPublico() {
   }, []);
 
   return (
-    <div className="p-6">
+    <div className="p-6 pt-24">
       <h1 className="text-2xl font-bold mb-4">🏗️ Equipamentos Disponíveis</h1>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
